446ec247dbed408526de9d036c88360caabea5a2
kingssik <hyunsikkang25@gmail.com>
2023-07-27 20:47:24 +0900

    게시물 수정 기능 구현

--- src/com/koreaIT/java/AM/Main.java
+++ src/com/koreaIT/java/AM/Main.java
@@ -52,7 +52,6 @@ public class Main {
         }
       } else if (cmd.startsWith("article detail ")) {
         String[] cmdBits = cmd.split(" ");
-//        cmdBits[2]; // 1(게시글 번호)
         int id = Integer.parseInt(cmdBits[2]);  // "1" -> 1
         Article foundArticle = null;
 
@@ -74,6 +73,34 @@ public class Main {
         System.out.printf("날짜 : %s\n", foundArticle.regDate);
         System.out.printf("제목 : %s\n", foundArticle.title);
         System.out.printf("내용 : %s\n", foundArticle.body);
+
+      } else if (cmd.startsWith("article modify ")) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]);  // "1" -> 1
+        Article foundArticle = null;
+
+        for (int i = 0; i < articles.size(); i++) {
+          Article article = articles.get(i);
+
+          if (article.id == id) {
+            foundArticle = article;
+            break;
+          }
+        }
+
+        if (foundArticle == null) {
+          System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+          continue;
+        }
+
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        foundArticle.title = title;
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+        foundArticle.body = body;
+
+        System.out.printf("%d번 게시물이 수정되었습니다\n", id);
 
       } else if (cmd.startsWith("article delete ")) {
         String[] cmdBits = cmd.split(" ");
