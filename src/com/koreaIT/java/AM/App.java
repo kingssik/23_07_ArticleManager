a93178d76ec8266ffb58c2b7d2d47a49c6651235
kingssik <hyunsikkang25@gmail.com>
2023-08-03 19:41:31 +0900

    회원가입 기능 구현

--- src/com/koreaIT/java/AM/App.java
+++ src/com/koreaIT/java/AM/App.java
@@ -42,8 +42,18 @@ public class App {
         int id = members.size() + 1;
 
         String regDate = Util.getNowDateStr();
-        System.out.printf("로그인 아이디 : ");
-        String loginId = sc.nextLine();
+        String loginId = null;
+
+        while (true) {
+          System.out.printf("로그인 아이디 : ");
+          loginId = sc.nextLine();
+
+          if (isJoinableLoginId(loginId) == false) {
+            System.out.printf("%s(은)는 사용중인 아이디입니다\n", loginId);
+            continue;
+          }
+          break;
+        }
 
         String loginPw = null;
         String loginPwCheck = null;
@@ -58,7 +68,6 @@ public class App {
             System.out.printf("비밀번호를 다시 입력하세요\n");
             continue;
           }
-
           break;
         }
 
@@ -173,6 +182,27 @@ public class App {
 
     sc.close();
 
+  }
+
+  private boolean isJoinableLoginId(String loginId) {
+    int index = getMemberIndexByLoginId(loginId);
+
+    if (index == -1) {
+      return true;
+    }
+    return false;
+  }
+
+  private int getMemberIndexByLoginId(String loginId) {
+    int i = 0;
+
+    for (Member member : members) {
+      if (member.loginId.equals(loginId)) {
+        return i;
+      }
+      i++;
+    }
+    return -1;
   }
 
   private int getArticleIndexById(int id) {
